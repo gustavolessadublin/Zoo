1d4fc86f097c1f0425b7bdc21d075dc1ea5ba61e
Gustavo Lessa <gustavolessadublin@gmail.com>
2018-04-25 12:55:25 +0100

    Qualification Enum created and update of Employee and ZooKeeper classes.

--- src/cctZoo/models/employees/Employee.java
+++ src/cctZoo/models/employees/Employee.java
@@ -13,9 +13,31 @@ import cctZoo.models.Model;
  */
 public abstract class Employee extends Model{
     private static int lastId;
+    private String name;
+    private String gender;
     
-    public Employee() {
+    public Employee(String gender) {
         super(++lastId);
+        this.gender = gender;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+    
     
 }
--- src/cctZoo/models/employees/zooKeeper/ZooKeeper.java
+++ src/cctZoo/models/employees/zooKeeper/ZooKeeper.java
@@ -5,14 +5,65 @@
  */
 package cctZoo.models.employees.zooKeeper;
 
+import cctZoo.models.animals.interfaces.Animal;
 import cctZoo.models.employees.Employee;
+import java.util.ArrayList;
 
 /**
  *
- * @author rbsrafa
+ * @author Gustavo Lessa
  */
 public class ZooKeeper extends Employee{
 
+    ArrayList<Qualification> qualifications;
+    ArrayList<Animal> caredAnimals;
+    
+    public ZooKeeper(String gender){
+        super(gender);
+        qualifications = new ArrayList<>();
+        caredAnimals = new ArrayList<>();
+    }
+    
+    public ArrayList getQualifications(){
+        return qualifications;
+    }
+    
+    public void addQualification(Qualification q){
+        if (qualifications.size()<3){
+            qualifications.add(q);
+        } else {
+            System.out.println("Keeper has already 3 qualifications.");
+        }
+    }
+    
+    public void removeQualification(Qualification q){
+        if (qualifications.contains(q)){
+            qualifications.remove(q);
+        } else {
+            System.out.println("The keeper does not have the qualification.");
+        }
+    }
+    
+    public void addAnimal(Animal a){
+        if(caredAnimals.size()<10){
+            caredAnimals.add(a);
+        } else {
+            System.out.println("Keeper is already taking care of 10 animals");
+        }
+    }
+    
+    public ArrayList getAnimals(){
+        return caredAnimals;
+    }
+    
+    public void removeAnimal(Animal a){
+        if(caredAnimals.contains(a)){
+            caredAnimals.remove(a);
+        } else {
+            System.out.println("This keeper is not taking care of this animal.");
+        }
+    }
+    
     @Override
     public String toString() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
