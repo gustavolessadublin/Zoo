70ba0dd8347cf47abcddd5328f85195e5425ed55
lucival1 <lucival1@gmail.com>
2018-05-08 16:33:33 +0100

    First Offspring work

--- src/cctZoo/models/animals/GenericReptile.java
+++ src/cctZoo/models/animals/GenericReptile.java
@@ -22,4 +22,16 @@ public class GenericReptile extends Animal implements Reptile{
         super(species, gender);
     }
     
+    public GenericReptile(String species, String gender, Animal offspring1, Animal offspring2) {
+        super(species, gender, offspring1, offspring2);
+    }
+    
+    public GenericReptile(String species, String gender, Animal offspring1) {
+        super(species, gender, offspring1);
+    }
+    
+    public GenericReptile(String species, String gender, boolean isOffspring) {
+        super(species, gender, isOffspring);
+    }
+    
 }
